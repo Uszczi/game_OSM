54bfd19c54da34c462137038fe9b7d48a696621d
Andrew2a1 <adam.brz@onet.eu>
2021-01-14 17:39:51 +0100

    Change GraphicObject interface

--- src/gui/GraphicObject.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * GraphicObject.cpp
- *
- *  Created on: 13 sty 2021
- *      Author: Adam
- */
-
-#include "GraphicObject.h"
--- src/gui/GraphicObject.h
+++ src/gui/GraphicObject.h
@@ -9,12 +9,18 @@
 #define GUI_GRAPHICOBJECT_H_
 
 #include "../hardware/PaintDevice.h"
+#include "../misc/Rect.h"
 
 class GraphicObject
 {
 public:
 	virtual ~GraphicObject() = default;
 	virtual void drawTo(PaintDevice &paintDevice) const = 0;
+	virtual Rect boundingRect() const = 0;
+
+	bool collidesWith(const GraphicObject& other) const {
+		return this->boundingRect().intersects(other.boundingRect());
+	}
 };
 
 #endif /* GUI_GRAPHICOBJECT_H_ */
